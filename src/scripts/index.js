074e6ec04b1ff3211582d96e9fde1cfcf81e100b
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2020-10-14 13:31:36 +0200

    [refactor]Delete unnecessary test js code
    This part of js code was necessary for me only to see of styles for
    mobile menu look well

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,8 +1,1 @@
 import '../styles/main.scss';
-
-const btn = document.querySelector('.mainMenuWrapper__btnCollapse');
-const nav = document.querySelector('.mainMenu');
-
-btn.addEventListener('click', ()=>{
-    nav.classList.toggle('mainMenu--open');
-})
