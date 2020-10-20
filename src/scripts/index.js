11138b6c3631ec999023fd0952df15fb316ca101
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2020-10-20 14:48:44 +0200

    Create soma variables
    These variables are necessary for working on mobileMenuHandler

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,5 +1,10 @@
 import '../styles/main.scss';
 import './components/mobileMenuHandler.js';
 
+//variables related with "mobileMenuHandler"
+const buttonCollapse = document.querySelector('#btnCollapse');
+const elementToToggle = document.querySelector('#menu');
+const classNameToggle = 'mainMenu--open';
+
 //COMPONENTS IMPORTS
 import MobileMenuHandler from './components/mobileMenuHandler.js';
