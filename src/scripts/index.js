af32f266aa0ab664d595086575ab3e1cd19c29a7
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2020-11-01 22:22:39 +0100

    Create new variables
    These two variables are related with DOM. It will be pass to
    DotsCreator(CreateDots.js) because in this component I have to work with
    DOM using these two elements

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -6,6 +6,10 @@ const buttonCollapse = document.querySelector('#btnCollapse');
 const elementToToggle = document.querySelector('#menu');
 const classNameToggle = 'mainMenu--open';
 
+//variables related with "CreateDots"
+const dotsWrapper = document.querySelector('#dotsWrapper');
+const sliderPhotos = document.querySelectorAll('.sliderWrapper__img');
+
 //COMPONENTS IMPORTS
 import MobileMenuHandler from './components/mobileMenuHandler.js';
 
