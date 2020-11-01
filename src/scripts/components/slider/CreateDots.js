aabad9086ecbcae5901d0921329f87dd12ac33d3
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2020-11-01 22:16:42 +0100

    Create a new component
    This component will be responsible for drawing dots depending on the
    amount of photos

---
+++ src/scripts/components/slider/CreateDots.js
@@ -0,0 +1,8 @@
+class DotsCreator {
+    constructor(dotsWrapper, sliderImgCollection) {
+        this.dotsWrapper = dotsWrapper;
+        this.sliderImgCollection = sliderImgCollection;
+    }
+
+}
+export default DotsCreator;
