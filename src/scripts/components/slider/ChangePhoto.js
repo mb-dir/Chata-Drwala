0bc5140734a7b5c39b4b761b94a25e6ba68d1245
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2020-11-05 13:21:16 +0100

    Add properties related with component operation

--- src/scripts/components/slider/ChangePhoto.js
+++ src/scripts/components/slider/ChangePhoto.js
@@ -4,6 +4,7 @@ class ChangePhoto {
         // dots wrapper because the dots are dynamically created and I cannot pass here the collection of dots
         // I mean when I want to save dots in variable like document.querySelectorAll(dots) I get an error, becuase there are no dots in DOM
         this.dotsWrapper = dotsWrapper;
+        this.index = 0;
     }
 }
 export default ChangePhoto;
