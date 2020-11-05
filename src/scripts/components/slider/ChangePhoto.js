dc1894721f51345beabf8a5036b83c6f33c88027
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2020-11-05 13:18:19 +0100

    Add properties to constructor

--- src/scripts/components/slider/ChangePhoto.js
+++ src/scripts/components/slider/ChangePhoto.js
@@ -1,5 +1,9 @@
 class ChangePhoto {
-    constructor() {
+    constructor(photos, dotsWrapper) {
+        this.photos = photos;
+        // dots wrapper because the dots are dynamically created and I cannot pass here the collection of dots
+        // I mean when I want to save dots in variable like document.querySelectorAll(dots) I get an error, becuase there are no dots in DOM
+        this.dotsWrapper = dotsWrapper;
     }
 }
 export default ChangePhoto;
