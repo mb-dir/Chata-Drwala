754943b55cea5b8088e9fbd32d74993c04e0c661
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2020-11-05 13:29:10 +0100

    Add new method
    This method is responsible for default photo changing(without user's
    action). Now this method only change photo, In the next step I am going
    to add the same feature for dots()

--- src/scripts/components/slider/ChangePhoto.js
+++ src/scripts/components/slider/ChangePhoto.js
@@ -5,6 +5,22 @@ class ChangePhoto {
         // I mean when I want to save dots in variable like document.querySelectorAll(dots) I get an error, becuase there are no dots in DOM
         this.dotsWrapper = dotsWrapper;
         this.index = 0;
+
+        this.defaultPhotoChange();
+    }
+
+    defaultPhotoChange(){
+        setInterval(() => {
+            this.index++;
+            if (this.index === this.photos.length) {
+                this.index = 0;
+            }
+            //photo change
+            for (const photo of this.photos) {
+                photo.classList.remove('sliderWrapper__img--active');
+            }
+            this.photos[this.index].classList.add('sliderWrapper__img--active');
+        }, 5000);
     }
 }
 export default ChangePhoto;
