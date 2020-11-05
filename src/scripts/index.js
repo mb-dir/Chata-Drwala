2c6ba9d5022b10b234484aee6a8ca22ec798b09d
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2020-11-05 13:15:19 +0100

    Import new component to index

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -13,6 +13,7 @@ const sliderPhotos = document.querySelectorAll('.sliderWrapper__img');
 //COMPONENTS IMPORTS
 import MobileMenuHandler from './components/mobileMenuHandler.js';
 import DotsCreator from './components/slider/CreateDots.js';
+import ChangePhoto from './components/slider/ChangePhoto';
 
 //COMPONENTS USE
 
