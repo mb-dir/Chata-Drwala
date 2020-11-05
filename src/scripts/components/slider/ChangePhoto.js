cfe3d4e15d9529c898ab8a727065209e4f0c1e2a
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2020-11-05 13:32:15 +0100

    Change the properties order
    I want to the order to be the same as in dots creator, because these two
    components use the same variables

--- src/scripts/components/slider/ChangePhoto.js
+++ src/scripts/components/slider/ChangePhoto.js
@@ -1,9 +1,9 @@
 class ChangePhoto {
-    constructor(photos, dotsWrapper) {
-        this.photos = photos;
+    constructor(dotsWrapper, photos) {
         // dots wrapper because the dots are dynamically created and I cannot pass here the collection of dots
         // I mean when I want to save dots in variable like document.querySelectorAll(dots) I get an error, becuase there are no dots in DOM
         this.dotsWrapper = dotsWrapper;
+        this.photos = photos;
         this.index = 0;
 
         this.defaultPhotoChange();
