c0d36822c43daf818109ae9fa4020215a7d8ee83
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2020-11-06 21:17:28 +0100

    Solving the problem of code repetition
    There was the same code in two places, so I create an auxiliary method
    with this code and now I only call this method in these two places

--- src/scripts/components/slider/ChangePhoto.js
+++ src/scripts/components/slider/ChangePhoto.js
@@ -17,40 +17,33 @@ class ChangePhoto {
             if (this.index === this.photos.length) {
                 this.index = 0;
             }
-            //photo change
-            for (const photo of this.photos) {
-                photo.classList.remove('sliderWrapper__img--active');
-            }
-            this.photos[this.index].classList.add('sliderWrapper__img--active');
             
-            //dots change
-            for (const dot of this.dots) {
-                dot.classList.remove('sliderWrapper__dot--active');
-            }
-            this.dots[this.index].classList.add('sliderWrapper__dot--active');
+            this.photoChange();
         }, 5000);
     }
     dotClickPhotoChange(){
         //convert to array to forEach works
         [...this.dots].forEach((dot, i)=>{
             dot.addEventListener('click', () => {
-
                 this.index = i;
-
-                //photo change
-                for (const photo of this.photos) {
-                    photo.classList.remove('sliderWrapper__img--active');
-                }
-                this.photos[this.index].classList.add('sliderWrapper__img--active');
-
-                //dots change
-                for (const dot of this.dots) {
-                    dot.classList.remove('sliderWrapper__dot--active');
-                }
-                this.dots[this.index].classList.add('sliderWrapper__dot--active');
-
+                this.photoChange();
             });
         });
+    }
+
+    //auxiliary methods
+    photoChange() {
+        //photo change
+        for (const photo of this.photos) {
+            photo.classList.remove('sliderWrapper__img--active');
+        }
+        this.photos[this.index].classList.add('sliderWrapper__img--active');
+
+        //dots change
+        for (const dot of this.dots) {
+            dot.classList.remove('sliderWrapper__dot--active');
+        }
+        this.dots[this.index].classList.add('sliderWrapper__dot--active');
     }
 }
 export default ChangePhoto;
