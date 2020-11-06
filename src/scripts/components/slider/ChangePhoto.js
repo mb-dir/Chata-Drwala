ea5e286b7b15fc03e3e9b15a0a528b0766fb263f
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2020-11-06 20:42:12 +0100

    Add new property
    The dots are dynamically created so to "catch" them I have to refer to
    dotsWrapper

--- src/scripts/components/slider/ChangePhoto.js
+++ src/scripts/components/slider/ChangePhoto.js
@@ -3,6 +3,7 @@ class ChangePhoto {
         // dots wrapper because the dots are dynamically created and I cannot pass here the collection of dots
         // I mean when I want to save dots in variable like document.querySelectorAll(dots) I get an error, becuase there are no dots in DOM
         this.dotsWrapper = dotsWrapper;
+        this.dots = this.dotsWrapper.children;
         this.photos = photos;
         this.index = 0;
 
