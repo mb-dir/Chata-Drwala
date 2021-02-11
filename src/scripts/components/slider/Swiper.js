366bff209f1e0df44dd97d1380619d850ba71c2c
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2021-02-11 21:16:01 +0100

    Get touchstart coordinates

--- src/scripts/components/slider/Swiper.js
+++ src/scripts/components/slider/Swiper.js
@@ -10,7 +10,8 @@ class Swiper{
     detectSwipeDirection(){
         for(const img of this.sliderImgs){
             img.addEventListener("touchstart",(e)=>{
-
+                this.swipeX = e.touches[0].clientX;
+                this.swipeY = e.touches[0].clientY;
             });
 
             img.addEventListener("touchmove",(e)=>{
