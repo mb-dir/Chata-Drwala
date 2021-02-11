541ad87b37515f0e5dfcff1255f524a575e60030
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2021-02-11 21:23:18 +0100

    Delete unnecessary coordinate
    Only clientX is needed for me

--- src/scripts/components/slider/Swiper.js
+++ src/scripts/components/slider/Swiper.js
@@ -3,7 +3,6 @@ class Swiper{
         this.sliderImgs = sliderImgs;
 
         this.swipeX = null;
-        this.swipeY = null;
 
         this.detectSwipeDirection();
     }
@@ -11,7 +10,6 @@ class Swiper{
         for(const img of this.sliderImgs){
             img.addEventListener("touchstart",(e)=>{
                 this.swipeX = e.touches[0].clientX;
-                this.swipeY = e.touches[0].clientY;
             });
 
             img.addEventListener("touchmove",(e)=>{
